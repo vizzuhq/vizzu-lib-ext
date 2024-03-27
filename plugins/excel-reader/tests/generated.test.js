ba9cf14567bfb772ec1aea1898211e112355012c
dovicsin <tamas.dovicsin@gmail.com>
2024-03-27 12:04:27 +0100

    check test directory exists and create

--- plugins/excel-reader/tests/generated.test.js
+++ plugins/excel-reader/tests/generated.test.js
@@ -7,10 +7,14 @@ import { globSync } from 'glob'
 import { ExcelReader } from '../src'
 import { generatedCases } from './assets/generated'
 
+const excelDirecotry = path.join(__dirname, '/assets/fixtures/excel')
+
+if (!fs.existsSync(excelDirecotry)) fs.mkdirSync(excelDirecotry)
+
 const mergedWB = XLSX.utils.book_new()
 globSync(__dirname + '/assets/fixtures/**/**.csv').forEach((file) => {
 	const fileName = path.parse(file).name
-	let destination = path.join(__dirname, `/assets/fixtures/excel/${fileName}.xlsx`)
+	let destination = path.join(excelDirecotry, `${fileName}.xlsx`)
 
 	if (fs.existsSync(destination)) fs.unlinkSync(destination)
 
@@ -22,20 +26,17 @@ globSync(__dirname + '/assets/fixtures/**/**.csv').forEach((file) => {
 	XLSX.utils.book_append_sheet(mergedWB, worksheet, fileName)
 	XLSX.writeFile(workbook, destination)
 })
-XLSX.writeFile(mergedWB, path.join(__dirname, `/assets/fixtures/excel/merged.xlsx`))
+XLSX.writeFile(mergedWB, path.join(excelDirecotry, `merged.xlsx`))
 
 describe('generated excel read', () => {
 	test.each(generatedCases())(
 		'test file: $fileName',
 		({ fileName, expected, detected, sheet = 0 }) => {
 			const excelReader = new ExcelReader()
-			const response = excelReader.readContent(
-				`${__dirname}/assets/fixtures/excel/${fileName}`,
-				{
-					fileType: 'file',
-					sheet: sheet
-				}
-			)
+			const response = excelReader.readContent(`${excelDirecotry}/${fileName}`, {
+				fileType: 'file',
+				sheet: sheet
+			})
 
 			expect(detected).toEqual(excelReader.detected)
 			expect(response).toEqual(expected)
