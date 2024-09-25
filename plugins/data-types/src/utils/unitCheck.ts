96ffa5171e85ed8a405bff2599a7972998aec1ab
dovicsin <dovicsin.tamas@gmail.com>
2024-09-25 14:04:25 +0200

    Add more manual check in unit detect

--- plugins/data-types/src/utils/unitCheck.ts
+++ plugins/data-types/src/utils/unitCheck.ts
@@ -3,7 +3,48 @@ import { type TypedSeries } from '../index'
 import { clearValue } from './clearValue'
 
 export const unitCheck = (series: TypedSeries): void => {
-	const ignored = ['Q', 'SKU', 'ID', 'SKU', 'EAN', 'UPC', 'ISBN', 'GTIN', 'MPN', 'ASIN']
+	const ignored = [
+		'Q',
+		'SKU',
+		'ID',
+		'EAN',
+		'UPC',
+		'ISBN',
+		'GTIN',
+		'MPN',
+		'ASIN',
+		'UUID',
+		'GUID',
+		'Ref',
+		'SN',
+		'PID',
+		'SID',
+		'VID',
+		'CID',
+		'BID',
+		'RID',
+		'OID',
+		'GID',
+		'MAC',
+		'IP',
+		'ISBN-13',
+		'ISBN-10',
+		'PartNo',
+		'SerialNo',
+		'LOT',
+		'Batch',
+		'IMEI',
+		'ICCID',
+		'SSCC',
+		'BIN',
+		'DUNS',
+		'VAT',
+		'TIN',
+		'CIF',
+		'IBAN',
+		'SWIFT',
+		'BIC'
+	]
 	const suffixRegexp = /^\d+([\D]+)$/
 	const prefixRegexp = /^([\D]+)\d+$/
 
@@ -22,10 +63,12 @@ export const unitCheck = (series: TypedSeries): void => {
 	}
 
 	const unit = test[1]
+	const notContained = ['-', '_']
 	if (
 		!unit ||
 		ignored.includes(unit.toUpperCase()) ||
-		ignored.includes(series.name.toUpperCase())
+		ignored.includes(series.name.toUpperCase()) ||
+		notContained.some((e) => unit.includes(e))
 	)
 		return
 
